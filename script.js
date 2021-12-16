e840490be0d0d4d6381655b014b1eb8585c6c73f
Cleiton <cleitons835@gmail.com>
2021-12-16 00:45:57 -0300

    Seleciona cor

--- script.js
+++ script.js
@@ -41,3 +41,69 @@ function criaUl(){
 
 } 
 criaUl();
+
+let colorList = ['black', 'orange', 'green', 'red'];
+
+/*
+paleta1 = document.getElementById('black').addEventListener('click', selecionaCor);
+paleta2 = document.getElementById('orange').addEventListener('click', selecionaCor);
+paleta3 = document.getElementById('green').addEventListener('click', selecionaCor);
+paleta4 = document.getElementById('red').addEventListener('click', selecionaCor());
+*/
+
+
+
+
+
+let classeSelecionadaBlack =document.getElementById('black');
+classeSelecionadaBlack.addEventListener('click', (evento)=> {
+    document.getElementById('black').classList.remove('selected')
+    document.getElementById('orange').classList.remove('selected')
+    document.getElementById('green').classList.remove('selected')
+    document.getElementById('red').classList.remove('selected')
+    document.getElementById(evento.target.id).classList.add('selected')
+    
+
+});
+
+
+
+let classeSelecionadaOrange =document.getElementById('orange');
+classeSelecionadaOrange.addEventListener('click', (evento)=> {
+    document.getElementById('black').classList.remove('selected')
+    document.getElementById('orange').classList.remove('selected')
+    document.getElementById('green').classList.remove('selected')
+    document.getElementById('red').classList.remove('selected')
+    document.getElementById(evento.target.id).classList.add('selected')
+    
+    });
+
+let classeSelecionadaGreen =document.getElementById('green');
+classeSelecionadaGreen.addEventListener('click', (evento)=> {
+    document.getElementById('black').classList.remove('selected')
+    document.getElementById('orange').classList.remove('selected')
+    document.getElementById('green').classList.remove('selected')
+    document.getElementById('red').classList.remove('selected')
+    document.getElementById(evento.target.id).classList.add('selected')
+    });
+let classeSelecionadaRed =document.getElementById('red');
+classeSelecionadaRed.addEventListener('click', (evento)=> {
+    document.getElementById('black').classList.remove('selected')
+    document.getElementById('orange').classList.remove('selected')
+    document.getElementById('green').classList.remove('selected')
+    document.getElementById('red').classList.remove('selected')
+    document.getElementById(evento.target.id).classList.add('selected')
+    });
+
+   
+
+/*
+function selecionaCor() {
+    document.getElementById('black').classList.remove('selected')
+    document.getElementById('orange').classList.remove('selected')
+    document.getElementById('grenn').classList.remove('selected')
+    document.getElementById('red').classList.remove('selected')
+    classeSelecionada.classList.add('selected')
+    
+}
+*/
