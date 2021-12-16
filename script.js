c536cb9405e94ea829ead80f3f131e63ab8ca893
Cleiton <cleitons835@gmail.com>
2021-12-16 12:21:34 -0300

    Reoganizando addVeventListener

--- script.js
+++ script.js
@@ -42,25 +42,13 @@ function criaUl(){
 } 
 criaUl();
 
-let colorList = ['black', 'orange', 'green', 'red'];
-
-/*
-paleta1 = document.getElementById('black').addEventListener('click', selecionaCor);
-paleta2 = document.getElementById('orange').addEventListener('click', selecionaCor);
-paleta3 = document.getElementById('green').addEventListener('click', selecionaCor);
-paleta4 = document.getElementById('red').addEventListener('click', selecionaCor());
-*/
-
 
 
 
 
 let classeSelecionadaBlack =document.getElementById('black');
 classeSelecionadaBlack.addEventListener('click', (evento)=> {
-    document.getElementById('black').classList.remove('selected')
-    document.getElementById('orange').classList.remove('selected')
-    document.getElementById('green').classList.remove('selected')
-    document.getElementById('red').classList.remove('selected')
+    document.querySelectorAll('.selected')[0].classList.remove('selected')
     document.getElementById(evento.target.id).classList.add('selected')
     
 
@@ -70,40 +58,22 @@ classeSelecionadaBlack.addEventListener('click', (evento)=> {
 
 let classeSelecionadaOrange =document.getElementById('orange');
 classeSelecionadaOrange.addEventListener('click', (evento)=> {
-    document.getElementById('black').classList.remove('selected')
-    document.getElementById('orange').classList.remove('selected')
-    document.getElementById('green').classList.remove('selected')
-    document.getElementById('red').classList.remove('selected')
+    document.querySelectorAll('.selected')[0].classList.remove('selected')
     document.getElementById(evento.target.id).classList.add('selected')
     
     });
 
 let classeSelecionadaGreen =document.getElementById('green');
 classeSelecionadaGreen.addEventListener('click', (evento)=> {
-    document.getElementById('black').classList.remove('selected')
-    document.getElementById('orange').classList.remove('selected')
-    document.getElementById('green').classList.remove('selected')
-    document.getElementById('red').classList.remove('selected')
+    document.querySelectorAll('.selected')[0].classList.remove('selected')
     document.getElementById(evento.target.id).classList.add('selected')
     });
 let classeSelecionadaRed =document.getElementById('red');
 classeSelecionadaRed.addEventListener('click', (evento)=> {
-    document.getElementById('black').classList.remove('selected')
-    document.getElementById('orange').classList.remove('selected')
-    document.getElementById('green').classList.remove('selected')
-    document.getElementById('red').classList.remove('selected')
+    document.querySelectorAll('.selected')[0].classList.remove('selected')
     document.getElementById(evento.target.id).classList.add('selected')
     });
 
    
 
-/*
-function selecionaCor() {
-    document.getElementById('black').classList.remove('selected')
-    document.getElementById('orange').classList.remove('selected')
-    document.getElementById('grenn').classList.remove('selected')
-    document.getElementById('red').classList.remove('selected')
-    classeSelecionada.classList.add('selected')
-    
-}
-*/
+     
