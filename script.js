316f7e06f2d3132cffe7bf4acaec02f4e742ac1f
jose-cleiton <cleitons835@gmail.com>
2021-12-20 20:30:50 -0300

    Colorir pixels

--- script.js
+++ script.js
@@ -16,64 +16,80 @@ let sectionItems = [
 
 let sectionsList = document.querySelector('#pixel-board');
 let ingredientListPai = document.querySelector('#pixel-board');
-
+let enderecoPixelIndividual=[];
 
 function criaLi(sectionsAserCriado) {
-    for(index in ingredientItems){
-        let itemAtual= ingredientItems[index];
+    for(let i in ingredientItems){
+        let itemAtual= ingredientItems[i];
         let itemASerCriado = document.createElement('li');
         itemASerCriado.innerText=itemAtual;
+        itemASerCriado.addEventListener('click', pintar);
+        
         sectionsAserCriado.appendChild(itemASerCriado);
-        itemASerCriado.classList.add('pixel');
-    }
+        itemASerCriado.setAttribute('class', 'pixel');
+
+       
+                    
+     }
 
 }
 function criaUl(){
-    for (index in sectionItems){
-        let sectionsAtual =sectionItems[index];
+    for (let j in sectionItems){
+        let sectionsAtual =sectionItems[j];
         let sectionsAserCriado = document.createElement('ul');
         sectionsAserCriado.innerText=sectionsAtual;              
         sectionsList.appendChild(sectionsAserCriado);
-        sectionsAserCriado.classList.add('ingredients-list')
+        sectionsAserCriado.setAttribute('class', 'ingredients-list')
 
         criaLi(sectionsAserCriado);
     } 
-
+    console.log(enderecoPixelIndividual)
+     
 } 
 criaUl();
 
+let colors = ['black','orange','green','red']
+
+function selecionaCorID(cores){
+    let classeSelecionada
+    let listaID = []
+    for (cor of cores){
+        classeSelecionada = document.getElementById(cor);
+        listaID.push(classeSelecionada)
+    }
+
+    return listaID
+
+}
+
+function criaOuvidor(listaID){
+
+    for (elemento of listaID){
+
+        elemento.addEventListener('click', corSelecionada)
+        console.log(elemento)
 
 
+    }
+}
 
 
-let classeSelecionadaBlack =document.getElementById('black');
-classeSelecionadaBlack.addEventListener('click', (evento)=> {
-    document.querySelectorAll('.selected')[0].classList.remove('selected')
-    document.getElementById(evento.target.id).classList.add('selected')
-    
-
-});
+let listaID = selecionaCorID(colors)
+console.log(listaID)
+criaOuvidor(listaID)
 
 
+    function corSelecionada(){
 
-let classeSelecionadaOrange =document.getElementById('orange');
-classeSelecionadaOrange.addEventListener('click', (evento)=> {
-    document.querySelectorAll('.selected')[0].classList.remove('selected')
-    document.getElementById(evento.target.id).classList.add('selected')
-    
-    });
+        document.querySelectorAll('.selected')[0].classList.remove('selected')
+        this.classList.add('selected')
+        
+    }
 
-let classeSelecionadaGreen =document.getElementById('green');
-classeSelecionadaGreen.addEventListener('click', (evento)=> {
-    document.querySelectorAll('.selected')[0].classList.remove('selected')
-    document.getElementById(evento.target.id).classList.add('selected')
-    });
-let classeSelecionadaRed =document.getElementById('red');
-classeSelecionadaRed.addEventListener('click', (evento)=> {
-    document.querySelectorAll('.selected')[0].classList.remove('selected')
-    document.getElementById(evento.target.id).classList.add('selected')
-    });
 
-   
+ function pintar() {
 
-     
+   this.style.backgroundColor =document.querySelectorAll('.selected')[0].id
+
+
+ }
